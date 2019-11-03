0dee07c3d9eaf0b022ec9ff878aaf4f1150e5df4
Marilyn Harbert <31261658+marilynharbert@users.noreply.github.com>
2019-11-03 00:18:46 -0400

    Cleaned up code and added comments

--- WebScrappingMayoralData.py
+++ WebScrappingMayoralData.py
@@ -1,56 +1,30 @@
-
-# coding: utf-8
-
-# In[130]:
-
-
-#import requests
-#import lxml.html as lh
 import pandas as pd
 from datetime import datetime
-#from bs4 import BeautifulSoup
-#import urllib.request as urllib
 
-
-# In[131]:
-
-
+# pull current year to update the url call based on the year
 currentYear = datetime.now().year
+
+# check to make sure we have the correct year
 print (currentYear)
 
-#####   UPDATE URL HERE    #########
+# If the URL needs updating, update it below
 url ='https://cawp.rutgers.edu/levels_of_office/women-mayors-us-cities-'+str(currentYear)
 
+# check to make sure we have the correct url
 print(url)
 
-
-# In[136]:
-
-
+# read the url into a dataframe
 dfs = pd.read_html(url,header=0, match='Vivian Jones', encoding='utf-8')
-
-#encoding  match='Vivian Jones'
 
 print(len(dfs[0]))
 
 for df in dfs:
     print(df)
 
-
-# In[135]:
-
-
 dfs[0]
 
-
-# In[138]:
-
-
+# print dataframe to csv
 dfs[0].to_csv(str(currentYear)+"mayorsover30000.csv", sep=',', encoding='utf-8')
-
-
-# In[143]:
-
 
 readme = open("READ_ME","w+")
 readme.write("You will need to find a few names with accents and hand-fix the weird characters that get substituted for the letters with accents. Additionally, the 'Rank' column is for the rank of the city in terms of population, that's why there are so many numbers missing. Otherwise you should be good to go!\n If you have additional questions email me at harbert.marilyn@gmail.com\n-Marilyn Harbert")
